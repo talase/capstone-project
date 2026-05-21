3de945855861688cc61a8a56a1c9fa8bbf12a951
reham4918 <rehamzakaria4918@gmail.com>
2026-05-21 14:15:04 +0300

    change

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,19 +1,24 @@
 from fastapi import FastAPI
 
 from app.config import load_env_file
-from app.routes.calendar import router as calendar_router
-from app.routes.style import router as style_router
-from app.model_pred.model import router as model_pred_router
 
+
+#NOTE: comment the parts you are not working on (model, calender, style)
+from app.routes.calendar import router as calendar_router
+app.include_router(calendar_router)
+
+from app.routes.style import router as style_router
+app.include_router(style_router)
+
+# from app.model_pred.model import router as model_pred_router
+# app.include_router(model_pred_router)
 
 
 load_env_file()
 
 app = FastAPI()
-app.include_router(model_pred_router)
 
-app.include_router(calendar_router)
-app.include_router(style_router)
+
 
 
 @app.get("/")
